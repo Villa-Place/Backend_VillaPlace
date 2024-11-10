1183ab81434b528a10cc231f5203e76d51750cee
rrdentin <denti.dona@gmail.com>
2024-11-10 14:55:22 +0700

    update placement function

--- src/routes/villaRoutes.ts
+++ src/routes/villaRoutes.ts
@@ -13,6 +13,7 @@ const {
   uploadVillaImages,
   getVillaImages,
   deleteVillaImage,
+  changeVillaStatus,
 } = require("../controllers/villaController");
 
 router.get("/", getAllVillas);
@@ -27,5 +28,6 @@ router.post(
 );
 router.get("/:id/photos", getVillaImages);
 router.delete("/:id/photos/:photoId", deleteVillaImage);
+router.patch("/:id", verifyAdmin, changeVillaStatus);
 
 module.exports = router;
